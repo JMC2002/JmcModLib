d7c9d59eeff94066795077c44b0d9f6f73a101c6
MC J <jmc8@qq.com>
2025-11-27 01:10:33 +0800

    格式

--- Config/ConfigAttribute.cs
+++ Config/ConfigAttribute.cs
@@ -14,9 +14,8 @@ namespace JmcModLib.Config
     /// <param name="onChanged">配置变更时的额外回调方法名称，需要和字段/变量在同一个类中，接受参数为新赋的值，将会在实际修改值前调用（注：不需要写变更变量的操作）</param>
     /// <param name="group"> 配置所在的分组，默认为 DefaultGroup</param>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-    public sealed class ConfigAttribute(string displayName,
-                           string? onChanged = null,
-                           string group = ConfigAttribute.DefaultGroup) : Attribute
+    public sealed class ConfigAttribute(string displayName, string? onChanged = null,
+                                        string group = ConfigAttribute.DefaultGroup) : Attribute
     {
         /// <summary>
         /// 显示名（用于放在UI上以及作为json中的key）
