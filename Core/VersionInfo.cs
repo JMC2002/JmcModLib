d2cfdfef95a3a4bb1688092b4df2aa514dccf327
MC J <jmc8@qq.com>
2025-12-19 00:26:13 +0800

    1.4.0

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -5,7 +5,7 @@ namespace JmcModLib.Core
     internal static class VersionInfo
     {
         internal const string Name = "JmcModLib";
-        internal const string Version = "1.3.2";
+        internal const string Version = "1.4.0";
 
         internal static ModInfo modInfo;
         internal static string Tag => $"[{Name} v{Version}]";
